c7efe85ffebf029a930fe2052063ade3076f3c50
mattx <10520670+mattx433@users.noreply.github.com>
2024-09-16 22:58:07 +0200

    add cma size override

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@ set -e
 cd luckfox-sdk
 ln -sf project/cfg/BoardConfig_IPC/BoardConfig-SPI_NAND-Buildroot-RV1103_Luckfox_Pico_Mini_B-IPC.mk .BoardConfig.mk
 echo "export CONFIG_USE_PREBUILT_ROOTFS=y" >> .BoardConfig.mk
+echo "export RK_BOOTARGS_CMA_SIZE=1K" >> .BoardConfig.mk
 ./build.sh
