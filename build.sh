5bee64d725e28c2ea1551cca309632a16ab50755
mattx <10520670+mattx433@users.noreply.github.com>
2024-12-13 20:45:05 +0100

    don't edit our own config

--- build.sh
+++ build.sh
@@ -2,6 +2,4 @@
 set -e
 cd luckfox-sdk
 ln -sf "$PWD/../BoardConfig-SD_CARD-Custom-RV1103_Luckfox_Pico_Mini_A-IPC.mk" .BoardConfig.mk
-echo "export CONFIG_USE_PREBUILT_ROOTFS=y" >> .BoardConfig.mk
-echo "export RK_BOOTARGS_CMA_SIZE=1K" >> .BoardConfig.mk
 ./build.sh
