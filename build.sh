655b1f0e8d925a44513871111c8680b04074f9ec
mattx <10520670+mattx433@users.noreply.github.com>
2024-12-13 22:08:58 +0100

    make hack patch the correct file

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ set -e
 cd luckfox-sdk
 ln -sf "$PWD/../BoardConfig-SD_CARD-Custom-RV1103_Luckfox_Pico_Mini_A-IPC.mk" .BoardConfig.mk
 # hack: workaround when using remainder of storage as rootfs
-sed -i 's/echo "0"/echo "128000000"/' build.sh
+sed -i 's/echo "0"/echo "128000000"/' project/build.sh
 ./build.sh
