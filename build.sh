e4559f2b96f1720b738e20ba121f2a6597431399
mattx <10520670+mattx433@users.noreply.github.com>
2025-04-22 12:23:36 +0200

    ignore build failures

--- build.sh
+++ build.sh
@@ -5,3 +5,5 @@ ln -sf "$PWD/../BoardConfig-SD_CARD-Custom-RV1103_Luckfox_Pico_Mini_A-IPC.mk" .B
 # hack: workaround when using remainder of storage as rootfs
 sed -i 's/echo "0"/echo "128000000"/' project/build.sh
 ./build.sh
+# hack: make it upload images even if the build fails
+exit 0
