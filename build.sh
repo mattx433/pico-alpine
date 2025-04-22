8e4aac80a44b8853c8c7b684d0de3bb5cd3e84c8
mattx <10520670+mattx433@users.noreply.github.com>
2025-04-22 12:26:05 +0200

    make it actually ignore errors

--- build.sh
+++ build.sh
@@ -4,6 +4,5 @@ cd luckfox-sdk
 ln -sf "$PWD/../BoardConfig-SD_CARD-Custom-RV1103_Luckfox_Pico_Mini_A-IPC.mk" .BoardConfig.mk
 # hack: workaround when using remainder of storage as rootfs
 sed -i 's/echo "0"/echo "128000000"/' project/build.sh
-./build.sh
 # hack: make it upload images even if the build fails
-exit 0
+./build.sh || exit 0
